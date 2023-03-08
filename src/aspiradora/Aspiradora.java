9afd50ea181971481f3f62ac5dda83037add7359
BryanDeAnda <C:/Users/Bryan De Anda/OneDrive/Documents/Computacin tolerante a fallas/Programa1/Programa1.py>
2023-03-08 16:49:06 -0600

    Primera parte

---
+++ src/aspiradora/Aspiradora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aspiradora;
+
+/**
+ *
+ * @author Bryan De Anda
+ */
+public class Aspiradora {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        Principal interfaz = new Principal();
+        interfaz.setVisible(true);
+    }
+    
+}
